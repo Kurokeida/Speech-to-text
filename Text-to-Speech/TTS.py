3a5ad015965bd2dd97874f6fad4a445a54d8eb02
心臓福本 <75956731+Kurokeida@users.noreply.github.com>
2024-02-15 14:42:05 -1000

    Add files via upload

---
+++ Text-to-Speech/TTS.py
@@ -0,0 +1,16 @@
+from playsound import playsound
+from datetime import datetime
+from gtts import gTTS
+
+now = datetime.now()
+dt_string = str(now.strftime("-%d-%m-%Y %H:%M:%S"))
+
+mytext = "The destroyer has arrived"
+
+audio = gTTS(mytext, lang='en', tld='co.uk')
+FileInfo = str("Speech-file"+dt_string+".mp4")
+print(type(FileInfo))
+FilePath = str("./Audio/"+FileInfo)
+audio.save(FilePath)
+
+playsound(FilePath)
